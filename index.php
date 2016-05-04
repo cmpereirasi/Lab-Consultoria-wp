ed2ff79aa9597467b320814aaf9343c7ac56ada5
cmpereirasi <cmpereira50@gmail.com>
2016-05-03 21:03:39 -0300

    criando tema filho seguindo estrutura wp

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lab Consultoria</title>
+    <title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
     <link rel="icon" href="img/Logo.png" type="image/gif">
 
     <!-- Bootstrap -->
